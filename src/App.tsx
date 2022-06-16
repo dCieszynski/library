4a49faa294515e9749fe38638817d9b753d4888a
Dawid <dcieszynski99@gmail.com>
2022-06-16 14:21:47 +0200

    add simple pagination

--- src/App.tsx
+++ src/App.tsx
@@ -56,17 +56,18 @@ export interface State {
 
 const App: React.FC = () => {
   const [books, setBooks] = useState<State["books"]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
 
-  const getBooksList = async (): Promise<BooksList> => {
+  const getBooksList = async (pageNum: number): Promise<BooksList> => {
     const response = await fetch(
-      "https://gnikdroy.pythonanywhere.com/api/book/"
+      `https://gnikdroy.pythonanywhere.com/api/book/?page=${pageNum}`
     );
     return await response.json();
   };
 
   useEffect(() => {
-    const getBooks = async () => {
-      const data: BooksList = await getBooksList();
+    const getBooks = async (pageNum: number) => {
+      const data: BooksList = await getBooksList(pageNum);
       const booksData: State["books"] = data.results.map((book) => {
         const images = book.resources.filter(
           (resource) => resource.type === "image/jpeg"
@@ -91,13 +92,55 @@ const App: React.FC = () => {
       console.log(booksData);
       setBooks(booksData);
     };
-    getBooks();
+    getBooks(currentPage);
   }, []);
+
+  useEffect(() => {
+    const getBooks = async (pageNum: number) => {
+      console.log(pageNum);
+      const data: BooksList = await getBooksList(pageNum);
+      const booksData: State["books"] = data.results.map((book) => {
+        const images = book.resources.filter(
+          (resource) => resource.type === "image/jpeg"
+        );
+        const [mediumImg] = images.filter((image) =>
+          image.uri.includes("medium")
+        );
+        const [source] = book.resources.filter(
+          (resource) => resource.type === "text/html; charset=utf-8"
+        );
+        const author = book.agents[0].person.replace(",", "");
+        return {
+          id: book.id,
+          title: book.title,
+          author: author,
+          subjects: book.subjects,
+          languages: book.languages,
+          mediumImg: mediumImg,
+          source: source,
+        };
+      });
+      console.log(booksData);
+      setBooks(booksData);
+    };
+    getBooks(currentPage);
+  }, [currentPage]);
+
+  const changePage = (page: number): void => {
+    if (currentPage + page != 0) {
+      let pageNum = currentPage;
+      pageNum += page;
+      console.log(pageNum);
+      setCurrentPage(pageNum);
+    }
+  };
 
   return (
     <div className="App">
       <h1>Library</h1>
       <List books={books}></List>
+      <button onClick={() => changePage(1)}>Next</button>
+      <button onClick={() => changePage(-1)}>Previous</button>
     </div>
   );
 };
